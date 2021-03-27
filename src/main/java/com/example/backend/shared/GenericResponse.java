85f2f5a6c103f1dd4e4a6d9f574bc4bbc51b1ea2
ugurcancaglayan <ugurcancaglayan@users.noreply.github.com>
2021-03-27 18:25:02 +0300

    GenericResponse created.

---
+++ src/main/java/com/example/backend/shared/GenericResponse.java
@@ -0,0 +1,14 @@
+package com.example.backend.shared;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GenericResponse {
+
+    private String message;
+
+}
