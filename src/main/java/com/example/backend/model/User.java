4c79982c589ce64f5381236a230bf4ef1e07c5a9
ugurcancaglayan <ugurcancaglayan@users.noreply.github.com>
2021-03-27 18:24:24 +0300

    User Entity added.

---
+++ src/main/java/com/example/backend/model/User.java
@@ -0,0 +1,32 @@
+package com.example.backend.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", nullable = false, length = 50, unique = true)
+    private String username;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    public User(String username, String firstName, String lastName) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
